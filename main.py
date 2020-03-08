13aa21660bff50873de8d8e235b57d06472141c7
pgibb96 <pgibb96@gmail.com>
2020-03-08 10:05:19 -0400

    Cleaning up the original next-train tracker and converting datatypes to datetime)

---
+++ main.py
@@ -0,0 +1,78 @@
+#Standard
+import requests
+import time
+import sys
+
+#Third Party
+from datetime import datetime, date
+
+class NextTrain():
+    def __init__(self):
+        self.number = 0
+
+    def determineNext(self):
+        # Setting the parameters to access the MBTA API data
+        URL = 'https://api-v3.mbta.com/schedules'
+        # Forest Hills
+        stop = 70001
+        route = 'Orange'
+        direction_id = 1
+        sort = 'departure_time'
+        limit = 1
+
+
+        now = datetime.now()
+        date = now.date()
+        # API requires HH:MM
+        min_time = f"{now.hour}:{now.minute}"
+
+        PARAMS = {'stop':stop,
+                  'route':route,
+                  'direction_id':direction_id,
+                  'min_time':min_time,
+                  'date':date,
+                  'sort':sort}
+
+        r = requests.get(URL, params = PARAMS)
+
+        # If you get anything other than a good request, flag and exit
+        if r.status_code != requests.codes.ok:
+            print("Error: Bad Request")
+            sys.exit()
+
+        data = r.json()
+
+        # I want to find a better way to access the info. To-do
+        next = data['data'][0]['attributes']['departure_time']
+
+        # If MBTA changes how their data is stored, this should be flagged here
+        if not next:
+            print("MBTA JSON format has changed: adjust appropriately")
+            sys.exit()
+
+        # Sending along the necessary info to the next stage
+        return [next, date, now]
+
+
+    def run(self):
+        next = self.determineNext()[0]
+        today = self.determineNext()[1]
+        time = self.determineNext()[2].replace(microsecond=0) #drop microsecond
+
+        # Converting the string from JSON into datetime type
+        nextDate = datetime.strptime(next.split("T")[0], '%Y-%m-%d').date()
+        nextTime = datetime.strptime(next.split("T")[1].split("-")[0], '%H:%M:%S').time()
+
+        next = datetime.combine(nextDate, nextTime)
+
+        difference = next - time
+        if difference.days < 0:
+            print("Wait for MBTA data to update")
+            sys.exit()
+        else:
+            print(f"Next Train at: {next.time()}({difference} left)")
+
+# Main Function
+if __name__ == "__main__":
+    temp = NextTrain()
+    temp.run()
