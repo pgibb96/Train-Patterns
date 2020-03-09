5bcd4b1118826559d74f9b4938f4d4b114a3a261
pgibb96 <pgibb96@gmail.com>
2020-03-09 07:56:26 -0400

    Added config method and connect method. Still need to test

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import requests
 import time
 import sys
 import config
+import psycopg2
+
 
 #Third Party
 from datetime import datetime, date
+from configparser import ConfigParser
 
-class NextTrain():
+
+
+class Info():
     def __init__(self):
         self.number = 0
 
@@ -98,7 +103,46 @@ class NextTrain():
 
         # Add Database Code Below
 
+    def config(filename='database.ini', section='postgresql'):
+        parser = ConfigParser()
+        parser.read(filename)
+
+        db = {}
+        if parser.has_section(section):
+            params = parser.items(section)
+            for params in params:
+                db[param[0]] = param[1]
+            else:
+                raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        return db
+
+    def connect():
+        conn = None
+
+        try:
+            params = self.config()
+
+            print('Connecting to the PostgreSQL database...')
+            conn = psycopg2.connect(**params)
+
+            cur = conn.cursor()
+
+            print('PostgreSQL database version:')
+            cur.execute('SELECT version()')
+
+            db_version = cur.fetchone()
+            print(db_version)
+
+            cur.close()
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+
 # Main Function
 if __name__ == "__main__":
-    temp = NextTrain()
+    temp = Info()
     temp.run()
+    connect()
